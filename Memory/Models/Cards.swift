74c766b1ba6a2650e7cd1bc746ca92e46b5a0f79
rauls4 <raul.silva@keylimetie.com>
2018-02-18 12:29:06 -0600

    Shuffled seed animal array so that every game would get a random set of pairs

--- Memory/Models/Cards.swift
+++ Memory/Models/Cards.swift
@@ -11,9 +11,11 @@ import Foundation
 struct Cards {
     var stack = [Card]()
     //This will hold our complete, shuffled stack of cards
-    private var animals = ["Bat","Cat","Cow","GhostDog","Hen","Horse","Pig","Spider","Octopus","Panda"]
+    private let animalsUnshuffled = ["Bat","Cat","Cow","GhostDog","Hen","Horse","Pig","Spider","Octopus","Panda"]
     
     init(totalCards:Int) {
+        //Shuffle the seed array so we don't get the same pairs every game
+        let animals = self.shuffleArray(array: animalsUnshuffled)
         //We need to indicate the total amount of cards so we don't end up with mistmatched cards on the deck
         if(totalCards > (animals.count * 2)){
             //This was occuring in the 4X5 grid (20 cards, with only 8 supplied animals, I added a couple more)
@@ -21,26 +23,28 @@ struct Cards {
         }else{
             let limitedAnimals = animals.enumerated().flatMap{ $0.offset < (totalCards/2) ? $0.element : nil } //limit pairs to our max
             for animal in limitedAnimals {
-                for _ in 0...1{
-                    let newCard = Card(type: animal, showingBack: true)
+                for _ in 0...1{//twice for each animal
+                    let newCard = Card(type: animal as! String, showingBack: true)
                     stack.append(newCard)
                     //add the new card to the deck
                 }
             }
-            self.shuffleStack()
+            stack = self.shuffleArray(array: stack) as! [Card]
             //and shuffle it
         }
     }
     
-    private mutating func shuffleStack(){
+    private mutating func shuffleArray(array:[Any]) -> [Any]{
         //this uses a modified Fisher-Yates algorithm to sort the array in place,
         //not really necessary for a maximum of 20 cards, but what the heck
-        var last = stack.count - 1
+        var last = array.count - 1
+        var array = array
         while(last > 0)
         {
             let rand = Int(arc4random_uniform(UInt32(last)))
-            stack.swapAt(last, rand)
+            array.swapAt(last, rand)
             last -= 1
         }
+        return array
     }
 }
